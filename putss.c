9187d9201ef2565f4e31b5b8012582ca86725b45
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-21 03:50:33 -0800

    fix code

--- putss.c
+++ putss.c
@@ -1,8 +1,14 @@
 #include "main.h"
+/**
+ * putss - function
+ * @str: str
+ * Return: count
+ */
 
 int putss(char *str)
 {
 	int count = 0;
+
 	while (str[count] != '\0')
 	{
 		_putchar(str[count]);
